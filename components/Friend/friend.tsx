15c7154c25df2eb6a72c31213224d18795dfc073
hriztam <hritamstark05@gmail.com>
2024-01-13 02:31:18 +0530

    Fixed the components issue and added components, context and Utils folder as well

---
+++ components/Friend/friend.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Friend = () => {
+  return (
+    <div>friend</div>
+  )
+}
+
+export default Friend
---
+++ components/UserCard/userCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserCard = () => {
+  return (
+    <div>userCard</div>
+  )
+}
+
+export default UserCard
