7c8d67a1928be2258b443d208c8eedb0854291cc
hriztam <hritamstark05@gmail.com>
2024-01-13 22:59:50 +0530

    Switched the whole project from Typescript to JavaScript

--- components/index.ts
+++
@@ -1,9 +0,0 @@
-import Error from "./Error/error";
-import Filter from "./Filter/filter";
-import Friend from "./Friend/friend";
-import Loader from "./Loader/loader";
-import Model from "./Model/model";
-import NavBar from "./NavBar/navBar";
-import UserCard from "./UserCard/userCard";
-
-export {Error, Filter, Friend, Loader, Model, NavBar, UserCard}
