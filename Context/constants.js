90574b7101a135cd62c312052b0571cb26171ad6
hriztam <hritamstark05@gmail.com>
2024-01-14 00:45:28 +0530

    Completed all the apiFeatures

--- Context/constants.js
+++ Context/constants.js
@@ -1,1 +1,8 @@
 // 0x5fbdb2315678afecb367f032d93f642f64180aa3
+import chatAppJSON from './ChatApp.json'
+
+export const ChatAppAddress = 
+"0x5fbdb2315678afecb367f032d93f642f64180aa3";
+
+
+export const ChatAppABI = chatAppJSON.abi;
